ad70b0204d108de656601a473bdbf87a649870a5
MaciekTalaska <maciek.talaska@gmail.com>
2018-06-07 19:57:43 +0200

    reading diceware lists

--- lib/dpgex.ex
+++ lib/dpgex.ex
@@ -15,4 +15,14 @@ defmodule Dpgex do
   def hello do
     :world
   end
+
+
+  @external_resource "priv/diceware-pl.txt"
+  @pl_diceware File.read! "priv/diceware-pl.txt"
+  def polish_diceware_list, do: @pl_diceware
+
+  @external_resource "priv/diceware-en.txt"
+  @en_diceware File.read! "priv/diceware-en.txt"
+  def english_diceware_list, do: @en_diceware
 end
+
