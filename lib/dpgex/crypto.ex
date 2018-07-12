0e32d6aef55898811b2c94c48c5b0639feb96047
MaciekTalaska <maciek.talaska@gmail.com>
2018-07-13 01:21:46 +0200

    remove throw_dice & throw_dices functions

--- lib/dpgex/crypto.ex
+++ lib/dpgex/crypto.ex
@@ -8,12 +8,4 @@ defmodule Dpgex.Crypto do
     Enum.to_list 1..count
       |> Enum.map(fn _ -> get_random_number(min, max) end)
   end
-
-  def throw_dice do
-    :crypto.rand_uniform 1, 6
-  end
-
-  def throw_dices(dices) do
-    Enum.to_list 1..dices |> Enum.map( fn _ -> :crypto.rand_uniform 1,6 end)
-  end
 end
