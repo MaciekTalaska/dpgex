2c3b0247f912d8ef93131d3a959c1bf23eaf4dd8
MaciekTalaska <maciek.talaska@gmail.com>
2018-07-12 19:45:20 +0200

    reading data - improvements

--- lib/dpgex/repository.ex
+++ lib/dpgex/repository.ex
@@ -7,18 +7,35 @@ defmodule Dpgex.DicewareRepository do
   @en_diceware File.read! "priv/diceware-en.txt"
   defp english_diceware_list, do: @en_diceware
 
-  defp get_polish_words do
-    polish_diceware_list()
+  defp get_words_from_resource_file(words) do
+    words
     |> String.split("\n")
+    |> Enum.map(fn x -> x |> String.split |> List.last end)
     |> Enum.drop(-1)
   end
 
-  defp get_english_words do
-    english_diceware_list()
-    |> String.split("\n")
-    |> Enum.map( fn x -> x |> String.split |> List.last  end)
-    |> Enum.drop(-1)
+  defp get_inner_diceware(lines) do
+    words = get_words_from_resource_file(lines)
+    # language is a key, so is not needed here
+    # language = "xx"
+    %{ # :language => language,
+       :length => Kernel.length(words),
+       :words => get_words_from_resource_file(lines)
+    }
   end
+
+#  defp get_polish_words do
+#    polish_diceware_list()
+#    |> String.split("\n")
+#    |> Enum.drop(-1)
+#  end
+#
+#  defp get_english_words do
+#    english_diceware_list()
+#    |> String.split("\n")
+#    |> Enum.map( fn x -> x |> String.split |> List.last  end)
+#    |> Enum.drop(-1)
+#  end
 
   def extract_language_from_filename(filename) do
     filename
@@ -38,6 +55,11 @@ defmodule Dpgex.DicewareRepository do
     files |> Enum.filter(fn x -> x |> String.contains?("diceware-") end)
   end
 
+  def get_supported_languages_from_files do
+    get_local_diceware_files()
+    |> Enum.map( fn f -> {f |> extract_language_from_filename(),f} end)
+  end
+
   def from_file(filename) do
     file = case File.read filename do
       {:error, reason } -> throw reason
@@ -52,8 +74,8 @@ defmodule Dpgex.DicewareRepository do
 
     {String.to_atom(language),
       %{:words => words,
-      :length => Kernel.length(words),
-      :language => language }}
+      :length => Kernel.length(words)}}
+      #:language => language }}
   end
 
   def create_repository_from_local_files do
@@ -63,29 +85,59 @@ defmodule Dpgex.DicewareRepository do
 
   def create_repository do
     inner = [
-      pl: %{language: "pl", words: get_polish_words(), length: Kernel.length(get_polish_words())},
-      en: %{language: "en", words: get_english_words(), length: Kernel.length(get_english_words())}
+      pl: %{ language: "pl",
+             # words: get_polish_words(),
+             words: get_words_from_resource_file(polish_diceware_list()),
+             length: Kernel.length(get_words_from_resource_file(polish_diceware_list()))},
+             # length: Kernel.length(get_polish_words())},
+      en: %{ language: "en",
+             #words: get_english_words(),
+             words: get_words_from_resource_file(english_diceware_list()),
+             length: Kernel.length(get_words_from_resource_file(english_diceware_list()))}
+             # length: Kernel.length(get_english_words())}
     ]
     local = create_repository_from_local_files()
     inner ++ local
   end
 
+  def get_language_data_from_file(language) do
+    languages = get_supported_languages_from_files()
+    exists = languages |> Enum.any?(fn ls -> elem(ls, 0) == language end)
+    case exists do
+      false -> {:error, :enoent}
+      true -> {:ok, from_file(elem(languages |> Enum.find(fn l -> elem(l,0)==language end),1)) }
+    end
+  end
 
   defp read_diceware_list(language) do
     case language do
-      "pl" -> { :ok, get_polish_words()}
-      "en" -> { :ok, get_english_words()}
-      _ -> {:error, :enoent}
+      "pl" -> {:ok,
+              get_inner_diceware(polish_diceware_list()) }
+      "en" -> {:ok,
+              get_inner_diceware(english_diceware_list())}
+       _ -> get_language_data_from_file(language)
     end
+#    case language do
+#      "pl" -> {
+#        :ok,
+#        get_words_from_resource_file(polish_diceware_list()) }
+#        #get_polish_words()}
+#        "en" -> {
+#        :ok,
+#        get_words_from_resource_file(english_diceware_list())}
+#      # get_english_words()}
+#      _ -> {:error, :enoent}
+#    end
   end
 
   def get_repository(language) do
     case read_diceware_list language do
-      {:ok, body} ->
-        words = body
-        length = words |> Kernel.length
-        language = language
-        %{words: words, length: length, language: language}
+      {:ok, body} -> body
+#      {:ok, body} ->
+#        words = body
+#        length = words |> Kernel.length
+#        language = language
+#        %{words: words, length: length, language: language}
         {:error, _} -> throw("No entry for language: '#{language}' found")
     end
   end
