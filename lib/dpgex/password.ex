5728393f94f8655b79c912e517c3dfe147422ee0
MaciekTalaska <maciek.talaska@gmail.com>
2018-07-13 17:34:31 +0200

    Crypto.get_random_elements for password generation

--- lib/dpgex/password.ex
+++ lib/dpgex/password.ex
@@ -9,8 +9,7 @@ defmodule Dpgex.Password do
   def get_random_words(language, words_count) do
     repository = Dpgex.DicewareRepository.get_repository language
     words = Map.get(repository, :words)
-    nums = Dpgex.Crypto.get_random_numbers(0, Map.get(repository, :length), words_count)
-    nums |> Enum.map(fn n -> Enum.at(words, n) end)
+    Dpgex.Crypto.get_random_elements(words, words_count)
   end
 
   def get_password(language, words_count, separator) do
@@ -20,6 +19,6 @@ defmodule Dpgex.Password do
 
   def get_all_passwords(language, words_count, separator, passwords_count) do
     Enum.to_list 1..passwords_count
-    |> Enum.map(fn x -> get_password(language, words_count, separator) end)
+    |> Enum.map(fn _ -> get_password(language, words_count, separator) end)
   end
 end
