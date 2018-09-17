3dea7b552299a09263accb08278b5a87d0bb28a8
MaciekTalaska <maciek.talaska@gmail.com>
2018-09-17 14:20:15 +0200

    more tests for Password module

--- test/lib/password_test.exs
+++ test/lib/password_test.exs
@@ -10,9 +10,25 @@ defmodule DpgexPasswordTest do
 
 
   test "get_random_words(_lang, X) should return X words" do
-    count = Dpgex.Crypto.get_random_number(0, 255)
+    count = Dpgex.Crypto.get_random_number(0, 10)
     words = Dpgex.Password.get_random_words("pl", count)
     assert length(words) == count
+  end
+
+  test "get_password should return specified number of words" do
+    count = Dpgex.Crypto.get_random_number(0, 10)
+    separator = "-"
+    password = Dpgex.Password.get_password("pl", count, separator)
+    words = String.split(password, separator)
+    assert length(words) == count
+  end
+
+  test "get_all_passwords should return required number of passwords" do
+    passwords_count = Dpgex.Crypto.get_random_number(0, 10)
+    password_length = Dpgex.Crypto.get_random_number(0, 10)
+    separator = "."
+    passwords = Dpgex.Password.get_all_passwords("pl", password_length, separator, passwords_count)
+    assert length(passwords) == passwords_count
   end
 end
 
